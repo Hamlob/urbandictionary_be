ceaab628bce377177853644d1fd0903b03a12e03
hamlob <jedlo.je.good@gmail.com>
2025-03-09 18:46:18 +0100

    same as above

--- project/posts/views.py
+++ project/posts/views.py
@@ -46,6 +46,7 @@ def random_post(request):
     max_id = Post.objects.latest('id').id
     random_id = randint(1,max_id)
     random_post = Post.objects.filter(id=random_id)
+    #check in case of deleted items
     while not random_post.exists() and random_id < max_id:
         random_id += 1
         random_post = Post.objects.filter(id=random_id)
