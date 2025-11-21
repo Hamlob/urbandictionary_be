c886d170f447b3943737811ef426585d8058fd4d
hamlob <jedlo.je.good@gmail.com>
2025-11-21 17:47:26 +0100

    add server IP to CSRF TRUSTED ORIGINS

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,7 +31,7 @@ if config["ENVIRONMENT"] in ("LOCAL_CONTAINER", "LOCAL"):
 else:
     DEBUG = False
     ALLOWED_HOSTS = [config["ALLOWED_HOST"]]
-    CSRF_TRUSTED_ORIGINS = ["https://urbandictionary.sk:8000"]
+    CSRF_TRUSTED_ORIGINS = ["https://urbandictionary.sk", config["SERVER_IP"]]
 
 
 # Application definition
